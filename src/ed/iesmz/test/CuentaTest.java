849ecaec592aa4f7d74c136596e1f53231a72a79
aangelcruz <aangelcruz@gmail.com>
2024-03-14 01:12:43 +0100

    inicio las pruebas  pasando los parametros de IBAN

--- src/ed/iesmz/test/CuentaTest.java
+++ src/ed/iesmz/test/CuentaTest.java
@@ -28,6 +28,23 @@ class CuentaTest {
     void compruebaIban4() {
         assertFalse(cuenta.compruebaIBAN("ES7600246912501234567891"));
     }
+    @Test
+    void compruebaIban5() {
+        assertFalse(cuenta.compruebaIBAN("ES4721000418401234567891"));
+    }
+
+    @Test
+    void compruebaIban6() {
+        assertFalse(cuenta.compruebaIBAN("ES8200491500051234567892"));
+    }
+
+
+    //pruebas pasando los parametros del iban
+
+    @Test
+    void generaIBAN1() {
+        assertEquals("ES7100302053091234567895", cuenta.generaIBAN("0030","2053","09","1234567895"));
+    }
 
 
 
